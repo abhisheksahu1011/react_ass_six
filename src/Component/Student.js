2aef86fb7b7ea887f673ca3d7bc5af24bff6d663
Abhishek sahu <100155445+abhisheksahu1011@users.noreply.github.com>
2022-05-28 15:17:04 +0530

    Update Student.js

--- src/Component/Student.js
+++ src/Component/Student.js
@@ -22,7 +22,7 @@ const Student = () => {
     <th scope="col">Name</th>
     <th scope="col">Age</th>
     <th scope="col">Course</th>
-    <th scope="col">Batch</th>
+    <th scope="col">Branch</th>
     <th scope="col">Change</th>
     </tr>
   </thead>
@@ -32,7 +32,7 @@ const Student = () => {
 <td>{student.Name}</td>
 <td>{student.Age}</td>
 <td>{student.Course}</td>
-<td>{student.Batch}</td>
+<td>{student.Branch}</td>
 <td><Link to="/edit">Edit</Link></td>
 
 </tr>
